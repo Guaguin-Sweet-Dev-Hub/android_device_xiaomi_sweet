a28cd7a51e2760df7c8feb34c7add8591ee873f6
bibin <bibindasappu1@gmail.com>
2021-05-01 15:33:38 +0530

    Sweet: cherish bring up

--- cherish_sweet.mk
+++ cherish_sweet.mk
@@ -7,14 +7,24 @@
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common cherish stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := cherish_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+#gapps
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+CHERISH_WITH_GAPPS := true
+
+# Maintainer Properties
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer= bibindasb
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
